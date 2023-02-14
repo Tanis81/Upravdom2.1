422518d094fbf61665a18e6b004dfe32881b6d7a
Tanis81 <121484019+Tanis81@users.noreply.github.com>
2023-02-14 18:39:27 +0300

    111

--- main.py
+++ main.py
@@ -1,6 +1,34 @@
 #Upravdom version 2.1 13/02/2023
 #creator Tanis
 #version for TSG Akimovskoe
+import sys
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+from PyQt6.QtCore import QSize, Qt
 
-class App:
-    pass
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("My App")
+        self.setFixedSize(QSize(400, 300))
+
+        # Set the central widget of the Window.
+
+
+        button = QPushButton("Press Me!")
+        button.setCheckable(True)
+        button.clicked.connect(self.the_button_was_clicked)
+        button.setFixedSize(QSize(100, 50))
+        # Set the central widget of the Window.
+        self.setCentralWidget(button)
+
+    def the_button_was_clicked(self):
+        print("Clicked!")
+
+
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+
+app.exec()
